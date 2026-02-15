d8380bed84cfe1f780497ef9c0a072c2ae30be8b
DevTec2020 <leonardo.silva.inf@gmail.com>
2026-02-14 23:57:50 -0300

    Feat: Adicionando funções

--- NGINSTAK/PortalDeVagasCurso.js
+++ NGINSTAK/PortalDeVagasCurso.js
@@ -11,7 +11,6 @@ function listarVagas(){
     alert(vagasEmTexto)
 }
 
-
 function novaVaga(){
     const nome = prompt("informe um nome para a vaga:")
     const descricao = prompt("Informe uma descrição para a vaga:")
@@ -26,4 +25,67 @@ function novaVaga(){
         vagas.push(novaVaga)
         alert("Vaga Criada.")
     }
+}
+
+function exibirVaga(){
+    const indice = prompt("Informe o indice da vaga que deseja consultar:")
+    const vaga = Vagas[indice]
+
+    const candidatosEmTexto = vaga.candidatos.reduce( function (textoFinal, candidado){
+        return textoFinal  + `\n ${candidado}`
+    },"")
+
+    alert(
+        `Vaga nº ${indice}
+        Nome: ${vaga.nome}
+        Descrição: ${vaga.descricao}
+        Data Limite: ${vaga.dataLimite}
+        Quantidade de Candidatos: ${vaga.candidatos.length}
+        Candidatos Inscritos: ${candidatosEmTexto}`
+    )
+}
+
+function inscreverCandidato(){
+    const candidato = prompt("Informe o nome do(a) candidato(a):")
+    const indice = prompt("Informe o indice da vaga para qual o(a) candidato(a) irá se inscrever:")
+    const vaga = vaga[indice]
+
+    const confirmacao = confirm(
+        `Deseja inscrever o(a) candidato(a) ${candidato} na vaga ${indice} Nome: ${vaga.nome} Descrição: ${vaga.descricao} Data Limite: ${vaga.dataLimite}`
+    )
+
+    if(confirmacao){
+        vaga.candidatos.push(candidato)
+        alert("Inscrição Realizada")
+    }
+}
+
+function excluirVaga(){
+    const indice = prompt("Informe o indice da vaga que deseja excluir:")
+    const vaga = Vagas[indice]   
+
+    const confirmacao = confirm(
+        `Tem certeza que deseja excluír a vaga:
+        ${indice} Nome: ${vaga.nome} Descrição: ${vaga.descricao} Data Limite: ${vaga.dataLimite}`
+    )
+
+    if(confirmacao){
+        Vagas.splice(indice,1)
+        alert("Vaga Excluída.")
+    }
+}
+
+function exibirMenu(){
+    const opcao = prompt(
+        `Bem Vindo ao Portal de Vagas\n
+        Escolha uma opção:
+        1- Listar Vagas 
+        2- Criar Nova Vaga
+        3- Visualizar Vaga
+        4- Inscrever Candidato
+        5- Excluir vaga
+        6- Sair`
+    )
+
+    return opcao
 }
